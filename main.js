2caeadf9e37e09ed37fde2d9a19545d9ac234e56
SARVAMBH <83635572+SARVAMBH@users.noreply.github.com>
2022-06-14 15:50:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function setup() {
     canvas = createCanvas(500,380);
     video = createCapture(VIDEO);
     video.size(490,500);
-    canvas.position(650,150);
+    canvas.position(550,250);
     video.position(705,140);
 }
